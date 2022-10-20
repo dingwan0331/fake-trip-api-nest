eb528ca438ec88634288f65f73b36e1b6953511f
dingwan <dingwan0331@gmail.com>
2022-10-20 15:32:27 +0900

    feat: accommodation entity 생성
    - product module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './config/typeOrm.config';
 import { AuthModule } from './apps/auth/auth.module';
+import { ProductModule } from './apps/product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
