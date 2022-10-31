d7e18bf1a9300d513ae0164d4aea26d76a296382
dingwan <dingwan0331@gmail.com>
2022-10-31 19:51:48 +0900

    refactor: 상세정도 호텔API customRepository 로직 삭제
    - custom repository 미지원으로 인한 해당 파일 주석처리
    - 해당 기능 service로직에 구현
    - proudct.modules imports 문 수정

--- src/apps/product/accommodation.repository.ts
+++ src/apps/product/accommodation.repository.ts
@@ -1,71 +1,71 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Accommodation } from './entities/accommodation.entity';
+// import { EntityRepository, Repository } from 'typeorm';
+// import { Accommodation } from './entities/accommodation.entity';
 
-@EntityRepository(Accommodation)
-export class AccommodationRepository extends Repository<Accommodation> {
-  async getAccommodation(id: string): Promise<Accommodation> {
-    const accommodationRow: Accommodation = await this.createQueryBuilder(
-      'accommodation',
-    )
-      .select()
-      .addSelect('room.price')
-      .leftJoinAndSelect('accommodation.accommodationType', 'type')
-      .leftJoinAndSelect('accommodation.accommodationRegion', 'region')
-      .leftJoinAndSelect('accommodation.accommodationSubImage', 'subImage')
-      .leftJoinAndSelect('accommodation.accommodationAmenity', 'amentiy')
-      .leftJoin('accommodation.room', 'room')
-      .orderBy({ price: 'ASC' })
-      .limit(1)
-      .where(id)
-      .getOne();
-    return accommodationRow;
-    // .addSelect('room.price')
-    // .addSelect(
-    //   (sq) =>
-    //     sq
-    //       .select('price')
-    //       .from('rooms', 'room')
-    //       .orderBy({ price: 'DESC' })
-    //       .limit(1),
-    //   'price',
-    // )
-    // .leftJoinAndSelect(
-    //   (sq) => {
-    //     return sq
-    //       .select(['accommodation_id', 'price'])
-    //       .from('rooms', 'room')
-    //       .orderBy({ price: 'ASC' })
-    //       .limit(1);
-    //   },
-    //   'room',
-    //   'room.accommodation_id = accommodation.id',
-    // )
-    // .leftJoinAndMapOne(
-    //   'accommodation.room',
-    //   (subQuery) => {
-    //     return subQuery.select('*').from('rooms', 'room');
-    //   },
-    //   'room',
-    //   'room.accommodation_id = accommodation.id',
-    // )
+// @EntityRepository(Accommodation)
+// export class AccommodationRepository extends Repository<Accommodation> {
+//   async getAccommodation(id: string): Promise<Accommodation> {
+// const accommodationRow: Accommodation = await this.createQueryBuilder(
+//   'accommodation',
+// )
+//   .select()
+//   .addSelect('room.price')
+//   .leftJoinAndSelect('accommodation.accommodationType', 'type')
+//   .leftJoinAndSelect('accommodation.accommodationRegion', 'region')
+//   .leftJoinAndSelect('accommodation.accommodationSubImage', 'subImage')
+//   .leftJoinAndSelect('accommodation.accommodationAmenity', 'amentiy')
+//   .leftJoin('accommodation.room', 'room')
+//   .orderBy({ price: 'ASC' })
+//   .limit(1)
+//   .where(id)
+//   .getOne();
+// return accommodationRow;
+//     // .addSelect('room.price')
+//     // .addSelect(
+//     //   (sq) =>
+//     //     sq
+//     //       .select('price')
+//     //       .from('rooms', 'room')
+//     //       .orderBy({ price: 'DESC' })
+//     //       .limit(1),
+//     //   'price',
+//     // )
+//     // .leftJoinAndSelect(
+//     //   (sq) => {
+//     //     return sq
+//     //       .select(['accommodation_id', 'price'])
+//     //       .from('rooms', 'room')
+//     //       .orderBy({ price: 'ASC' })
+//     //       .limit(1);
+//     //   },
+//     //   'room',
+//     //   'room.accommodation_id = accommodation.id',
+//     // )
+//     // .leftJoinAndMapOne(
+//     //   'accommodation.room',
+//     //   (subQuery) => {
+//     //     return subQuery.select('*').from('rooms', 'room');
+//     //   },
+//     //   'room',
+//     //   'room.accommodation_id = accommodation.id',
+//     // )
 
-    // await this.find({
-    //   where: id,
-    //   relations: [
-    //     'accommodationType',
-    //     'accommodationRegion',
-    //     'accommodationSubImage',
-    //     'accommodationAmenity',
-    //   ],
-    //   join: {
-    //     alias: 'room',
-    //     leftJoin: { room: 'a' },
-    //   },
-    // });
+//     // await this.find({
+//     //   where: id,
+//     //   relations: [
+//     //     'accommodationType',
+//     //     'accommodationRegion',
+//     //     'accommodationSubImage',
+//     //     'accommodationAmenity',
+//     //   ],
+//     //   join: {
+//     //     alias: 'room',
+//     //     leftJoin: { room: 'a' },
+//     //   },
+//     // });
 
-    // if (!accommodationRow) {
-    //   return undefined;
-    // }
-    // return accommodationRow[0];
-  }
-}
+//     // if (!accommodationRow) {
+//     //   return undefined;
+//     // }
+//     // return accommodationRow[0];
+//   }
+// }
