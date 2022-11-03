3d01d4da871b7eda672e992a5a03d87f2b953aa7
dingwan <dingwan0331@gmail.com>
2022-11-03 13:26:41 +0900

    feat: 리뷰 수정 API url설정

--- src/apps/review/review.controller.ts
+++ src/apps/review/review.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Param, Post, Put } from '@nestjs/common';
 
 @Controller('review')
 export class ReviewController {
   @Post('/reviews')
   async createReview() {
+    return;
+  }
+
+  @Put('/reviews/:id')
+  async updateReview(@Param() id: string) {
     return;
   }
 }
