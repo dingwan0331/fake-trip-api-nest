2ca8133adf541ef78cbcfe01af86a06eab35a1aa
dingwan <dingwan0331@gmail.com>
2022-11-03 13:06:26 +0900

    feat: 숙소 리스트 API url 설정

--- src/apps/product/product.controller.ts
+++ src/apps/product/product.controller.ts
@@ -4,6 +4,11 @@ import { ProductService } from './product.service';
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+  @Get('/accommodations')
+  async getAccommodations() {
+    return;
+  }
+
   @Get('/accommodations/:id')
   async getAccommodation(@Param() id: string) {
     return this.productService.getAccommodation(id);
