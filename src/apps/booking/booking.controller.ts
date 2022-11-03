b250fcbb8338b208f04cbcc2df647f135f315a10
dingwan <dingwan0331@gmail.com>
2022-11-03 13:43:41 +0900

    feat: 예약 화면 rooms API url생성

--- src/apps/booking/booking.controller.ts
+++ src/apps/booking/booking.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Get, Param } from '@nestjs/common';
 
 @Controller('booking')
 export class BookingController {
   @Post('/reservations')
   async createBooking() {
+    return;
+  }
+
+  @Get('/rooms/:id')
+  async getRoom(@Param() id: string) {
     return;
   }
 }
