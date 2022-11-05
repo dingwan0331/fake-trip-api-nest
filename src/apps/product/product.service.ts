ea4eff1ca0fd2011f60cfa26de2cdae9aed0b096
dingwan <dingwan0331@gmail.com>
2022-11-05 18:55:49 +0900

    feat: 상세페이지 rooms API 작성
    - 쿼리스트링 값 Dto로 작성
    - 예약불가 룸들은 return하지 않음
    - 쿼리스트링의 케이스가 다른건 구조분해할당으로 우선 처리

--- src/apps/product/product.service.ts
+++ src/apps/product/product.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { GetRoomsDto } from './dtos/get-rooms.dto';
 import { Accommodation } from './entities/accommodation.entity';
+import { Room } from './entities/room.entity';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Accommodation)
     private readonly accommodationRepository: Repository<Accommodation>,
+    @InjectRepository(Room)
+    private readonly roomRepository: Repository<Room>,
   ) {}
   async getAccommodation(id: string) {
     const accommodationRow: Accommodation = await this.accommodationRepository
@@ -26,5 +30,44 @@ export class ProductService {
     accommodationRow['price'] = accommodationRow.room[0].price;
     delete accommodationRow.room;
     return accommodationRow;
+  }
+
+  async getRooms(getRoomsDto: GetRoomsDto): Promise<Room[]> {
+    const {
+      'accommodation-id': accommodationId = 1,
+      'start-date': startDate = new Date(),
+      'end-date': endDate = new Date(),
+      guest = 2,
+    } = getRoomsDto;
+
+    const roomsRow: Room[] = await this.roomRepository
+      .createQueryBuilder('r')
+      .select()
+      .leftJoinAndSelect('r.roomSubImage', 'subImage')
+      .leftJoin(
+        (subQuery) => {
+          return subQuery
+            .select('b.room_id, Count(*) AS booking_count')
+            .from('bookings', 'b')
+            .leftJoin('b.room', 'r')
+            .where(
+              'r.accommodation_id = :accommodationId AND b.start_date < :endDate AND b.end_date > :startDate',
+              { accommodationId, startDate, endDate },
+            )
+            .groupBy('b.room_id');
+        },
+        'b',
+        'b.room_id = r.id',
+      )
+      .where(
+        'r.accommodation_id = :accommodationId AND r.quantity - IFNULL(b.booking_count,0) != 0 AND r.max_guest > :guest',
+        {
+          accommodationId,
+          guest,
+        },
+      )
+      .getMany();
+
+    return roomsRow;
   }
 }
