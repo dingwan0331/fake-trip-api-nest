31eedd31a6251986bb34e7a8d3bbdcca643db7e1
dingwan <dingwan0331@gmail.com>
2022-11-07 18:25:27 +0900

    refactor: TypeOrmModule 비동기 처리 및 ConfigModule 사용
    - typeOrm.config.ts configService를 활용하여 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { typeORMConfig } from './config/typeOrm.config';
+import { TypeOrmConfigService } from './config/typeOrm.config';
 import { AuthModule } from './apps/auth/auth.module';
 import { ProductModule } from './apps/product/product.module';
 import { ReviewModule } from './apps/review/review.module';
@@ -25,7 +25,9 @@ import * as Joi from 'joi';
         SERVER_PORT: Joi.string().default(8000),
       }),
     }),
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
     AuthModule,
     ProductModule,
     ReviewModule,
