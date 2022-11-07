c0049fea6ee0a547c7f35eb296ce32c173e82e55
dingwan <dingwan0331@gmail.com>
2022-11-07 18:25:27 +0900

    refactor: TypeOrmModule 비동기 처리 및 ConfigModule 사용
    - typeOrm.config.ts configService를 활용하여 사용

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -1,19 +1,23 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
-const env = process.env;
-
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: env.MYSQL_HOST,
-  port: 3306,
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  entities: [__dirname + '../../**/*.entity.{js,ts}'],
-  synchronize: Boolean(env.MYSQL_SYNC),
-  namingStrategy: new SnakeNamingStrategy(),
-  logging: true,
-};
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('MYSQL_HOST'),
+      port: 3306,
+      username: this.configService.get('MYSQL_USERNAME'),
+      password: this.configService.get('MYSQL_PASSWORD'),
+      database: this.configService.get('MYSQL_DATABASE'),
+      entities: [__dirname + '../../**/*.entity.{js,ts}'],
+      synchronize: this.configService.get('MYSQL_SYNC'),
+      namingStrategy: new SnakeNamingStrategy(),
+      logging: true,
+    };
+  }
+}
