78493111cba5c5a847525e13cc02ed3f44ebcec9
dingwan <dingwan0331@gmail.com>
2022-11-07 17:18:11 +0900

    refactor: ConfigModule validationSchema 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,23 @@ import { ProductModule } from './apps/product/product.module';
 import { ReviewModule } from './apps/review/review.module';
 import { BookingModule } from './apps/booking/booking.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_USERNAME: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+        MYSQL_SYNC: Joi.number().default(0),
+        JWT_EXPIRE_IN: Joi.number().required(),
+        SECRET_KEY: Joi.string().required(),
+        SERVER_PORT: Joi.string().default(8000),
+      }),
+    }),
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
     ProductModule,
