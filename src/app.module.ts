6d65105c9d786b899d06720cd2e6a312460262ea
dingwan <dingwan0331@gmail.com>
2022-11-07 17:09:19 +0900

    refactor: ConfigModule 기본설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { AuthModule } from './apps/auth/auth.module';
 import { ProductModule } from './apps/product/product.module';
 import { ReviewModule } from './apps/review/review.module';
 import { BookingModule } from './apps/booking/booking.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
     ProductModule,
