a2d5852beabee9a9bfc010df6ba3dde80a9839c3
dingwan <dingwan0331@gmail.com>
2022-11-11 18:30:04 +0900

    feat: 숙소리스트 API 작성
    - get-accommodation-dto 작성, 디폴트값 및 발리데이션 적용
    - 날짜와 숙박인원에 따른 예약가능한 호텔만 반환
    - 가격은 최저 가격만 반환

--- src/apps/product/product.repository.ts
+++ src/apps/product/product.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { GetAccommodationsDto } from './dtos/get-accommodations.dto';
 import { GetRoomsDto } from './dtos/get-rooms.dto';
 import { Accommodation } from './entities/accommodation.entity';
 import { Room } from './entities/room.entity';
@@ -62,5 +63,54 @@ export class ProductRepository {
       .getOne();
 
     return accommodationRow;
+  }
+
+  async getAccommodations(
+    getAccommodationsDto: GetAccommodationsDto,
+  ): Promise<any[]> {
+    const { startDate, endDate, guests, offset, limit, order } =
+      getAccommodationsDto;
+
+    const accommodationsRow = await this.accommodationRepository
+      .createQueryBuilder('a')
+      .select()
+      .addSelect('room.price', 'room_price')
+      .leftJoinAndSelect('a.accommodationType', 'type')
+      .leftJoinAndSelect('a.accommodationRegion', 'region')
+      .leftJoin(
+        (subQuery) => {
+          return subQuery
+            .select('Min(r.price) AS price, r.accommodation_id')
+            .from('rooms', 'r')
+            .leftJoin(
+              (subQuery) => {
+                return subQuery
+                  .select('b.room_id, Count(*) AS booking_count')
+                  .from('bookings', 'b')
+                  .where(
+                    'b.start_date < :endDate AND b.end_date > :startDate',
+                    { startDate, endDate },
+                  )
+                  .groupBy('b.room_id');
+              },
+              'b',
+              'r.id = b.room_id',
+            )
+            .where(
+              '(r.quantity - IFNULL(b.booking_count,0)) > 0 AND r.max_guest >= :guests',
+              { guests },
+            )
+            .groupBy('r.accommodation_id');
+        },
+        'room',
+        'a.id = room.accommodation_id',
+      )
+      .where('room.price IS NOT NULL')
+      .offset(offset)
+      .limit(limit)
+      .orderBy(order[0], order[1])
+      .getRawMany();
+
+    return accommodationsRow;
   }
 }
--- src/apps/product/product.service.ts
+++ src/apps/product/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { GetAccommodationsDto } from './dtos/get-accommodations.dto';
 import { GetRoomsDto } from './dtos/get-rooms.dto';
 import { Accommodation } from './entities/accommodation.entity';
 import { Room } from './entities/room.entity';
@@ -20,5 +21,33 @@ export class ProductService {
     const roomsRow: Room[] = await this.productRepository.getRooms(getRoomsDto);
 
     return roomsRow;
+  }
+
+  async getAccommodations(
+    getAccommodationsDto: GetAccommodationsDto,
+  ): Promise<any[]> {
+    const accommodationRow = await this.productRepository.getAccommodations(
+      getAccommodationsDto,
+    );
+
+    const accommodations = accommodationRow.map((row) => {
+      return {
+        id: row.a_id,
+        name: row.a_name,
+        grade: row.a_grade,
+        address: row.a_address,
+        description: row.a_description,
+        main_image_url: row.a_main_image_url,
+        thumnail: row.a_thumnail,
+        longtitude: row.a_longtitude,
+        latitude: row.a_latitude,
+        check_in: row.a_check_in,
+        check_out: row.a_check_out,
+        type: { id: row.type_id, type: row.type_type },
+        region: { id: row.region_id, region: row.region_region },
+        price: '280000.00',
+      };
+    });
+    return accommodations;
   }
 }
